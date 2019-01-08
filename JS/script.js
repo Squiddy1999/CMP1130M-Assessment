cf2e2d045de1a9be1297c24352762a5e9d8340f8
Squiddy1999 <43581430+Squiddy1999@users.noreply.github.com>
2019-01-08 15:52:16 +0000

    updated

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,33 @@ $(document).ready(function(){
     $('.flexslider').flexslider({
         animation: "slide"
     });
+    nowPlaying = "null";
+    customiserBtn();
+    musicPreview();
     console.log("Window Ready");
+    
 })
 
+function customiserBtn() {
+    $("#create_album_button_section ").mouseover(function () {
+        $(this).css("background-color", "#875b36")
+    });
+    $("#create_album_button_section ").mouseleave(function () {
+        $(this).css("background-color", "#DC965A")
+    });
+}
+
+function musicPreview() {    
+    $(".song").click(function () {
+        if (nowPlaying == "null") {
+            nowPlaying = $(this);
+            $(this).css("font-weight", "bold");
+        }
+        else {
+            $(nowPlaying).css("font-weight", "normal");
+            nowPlaying = $(this);
+            $(this).css("font-weight", "bold");
+        }
+        
+    });
+}
